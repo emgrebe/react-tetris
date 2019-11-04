dc56e7cca298804034444de4ab121c4fab481954
Emily Landgrebe <emilandgrebe@gmail.com>
2019-11-04 16:40:09 -0600

    cells created on page

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { createStage } from '../gameHelper'
 
 //Components
 import Stage from './Stage'
@@ -9,7 +10,9 @@ const Tetris = () => {
 
   return (
     <div>
-      <Stage />
+      <Stage 
+        stage={createStage()}
+      />
       <aside>
         <div>
           <Display text='Score' />
