939a7d8e152b2e0a114c441af6c53f36f56233c6
Emily Landgrebe <emilandgrebe@gmail.com>
2019-11-05 13:02:24 -0600

    random blocks show up after plessing the start button, and ability to move the blocks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import { createStage } from '../gameHelper'
+
 //Styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
 
@@ -16,14 +18,47 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null)
   const [gameOver, setGameOver] = useState(false)
 
-  const [player] = usePlayer()
+  const [player, updatePlayerPosition, resetPlayer] = usePlayer()
   const [stage, setStage] = useStage(player)
 
   
   console.log('re-render')
 
+  const movePlayer = direction => {
+    updatePlayerPosition({ x: direction, y: 0 })
+  }
+
+  const startGame = () => {
+    //Reset everything
+    setStage(createStage())
+    resetPlayer()
+  }
+
+  const drop = () => {
+    updatePlayerPosition({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop()
+  }
+
+  const move = ({ keyCode }) => {
+    if(!gameOver) {
+      if(keyCode === 37) {
+        movePlayer(-1)
+      } else if (keyCode === 39) {
+        movePlayer(1)
+      } else if (keyCode === 40) {
+        dropPlayer()
+      }
+    }
+  }
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper 
+      role='button' 
+      tabIndex='0' 
+      onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -38,7 +73,7 @@ const Tetris = () => {
               <Display text='Level' />
             </div>
           )}
-        <StartButton />
+        <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
